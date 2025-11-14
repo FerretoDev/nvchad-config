cf6e699aca42c165eacbe00d4009660c60d1d208
Marcos Ferreto <marcosferretoestrada@gmail.com>
2025-11-14 16:59:04 -0600

    update

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,5 +1,20 @@
 return {
     "github/copilot.vim",
     lazy = false,
+    config = function()
+        -- Habilitar Copilot en tipos de archivo de texto/Markdown
+        vim.g.copilot_filetypes = {
+            markdown = true,
+            ["markdown.mdx"] = true,
+            gitcommit = true,
+            text = true,
+            TelescopePrompt = false,
+        }
 
+        -- Navegar sugerencias de Copilot (opcional): Alt-[ y Alt-]
+        vim.api.nvim_set_keymap("i", "<M-]>", "copilot#Next()", { silent = true, expr = true })
+        vim.api.nvim_set_keymap("i", "<M-[>", "copilot#Previous()", { silent = true, expr = true })
+        -- Desplegar panel (opcional): <M-p>
+        vim.api.nvim_set_keymap("i", "<M-p>", "copilot#Panel()", { silent = true, expr = true })
+    end,
 }
